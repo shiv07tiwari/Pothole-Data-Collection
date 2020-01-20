7c6e3ff846dc1fdf6fe20b98b11c0829529d90a3
Shivansh <shivansh16tiwari@gmail.com>
2020-01-20 18:42:43 +0530

    added location service for latitude and longitude

--- app/src/main/java/com/example/potholedatacollection/home/view/MainActivity.kt
+++ app/src/main/java/com/example/potholedatacollection/home/view/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.potholedatacollection.home.view
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import com.example.potholedatacollection.R
+import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
 /**
  * TODO : Add location wherever sheet is updated. Sheet is basically database. Abhi phone me hi excel sheet bana denge
@@ -23,10 +36,25 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     lateinit var sensorGyroscope: Sensor
     var tripTimer: Int = 0
     var potHoleCounter: Int = 0
+    private val REQUEST_PERMISSION_LOCATION = 10
+    internal lateinit var mLocationRequest: LocationRequest
+    var latitude:Double = 0.0
+    var longitude:Double = 0.0
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+    lateinit var countDownTimer:CountDownTimer
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mLocationRequest = LocationRequest()
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps()
+        }
+
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         sensorGyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
@@ -46,12 +74,16 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     fun startNewTrip() {
+        Log.e("log","Starting new trip")
 
         if (isTripOn) {
             isTripOn = false
             btn_start.text = "Start"
             disableSensors()
             disablePotholeCounter()
+            stopLocationService()
+            countDownTimer.onFinish()
+            countDownTimer.cancel()
             return
         }
         tripTimer = 0
@@ -59,7 +91,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         isTripOn = true
         btn_start.text = "Trip On"
         btn_pothole.isEnabled = true
-        object : CountDownTimer(50000, 1000) {
+        countDownTimer = object : CountDownTimer(50000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 txt_trip_duration.text = "Trip time : "+tripTimer+" seconds"
                 tripTimer++
@@ -71,16 +103,82 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }.start()
         enableSensors()
         enablePotHoleCounter()
+        startLocationService()
+    }
+
+    fun startLocationService() {
+        Log.e("log","Starting Location services")
+        if (checkPermissionForLocation(this)) {
+            startLocationUpdates()
+        }
+    }
+
+    fun stopLocationService() {
+        Log.e("log","Stopping location service")
+        stoplocationUpdates()
+    }
+
+
+
+    protected fun startLocationUpdates() {
+
+        // Create the location request to start receiving updates
+
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 1000
+        mLocationRequest.fastestInterval = 500
+
+        // Create LocationSettingsRequest object using location request
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(mLocationRequest!!)
+        val locationSettingsRequest = builder.build()
+
+        val settingsClient = LocationServices.getSettingsClient(this)
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+            return
+        }
+        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback,
+            Looper.myLooper())
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            // do work here
+            locationResult.lastLocation
+            onLocationChanged(locationResult.lastLocation)
+        }
+    }
+    private fun stoplocationUpdates() {
+        mFusedLocationProviderClient!!.removeLocationUpdates(mLocationCallback)
+    }
+
+    fun onLocationChanged(location: Location) {
+        // New location has now been determined
+        latitude = location.latitude
+        longitude = location.longitude
+        Log.e("log","Location change : "+location.latitude+" "+location.longitude)
+
+        // You can now create a LatLng Object for use with maps
     }
 
 
     fun enableSensors() {
         /**We need sensor readings every 5th second */
+        Log.e("log","Sensors enabled")
+
         sensorManager.registerListener(this,sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
         sensorManager.registerListener(this,sensorGyroscope, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     fun disableSensors() {
+        Log.e("log","Sensors disabled")
+
         sensorManager.unregisterListener(this, sensorAccelerometer)
         sensorManager.unregisterListener(this,sensorGyroscope)
     }
@@ -111,7 +209,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 // To send data in interval of 5 seconds
                 // Add data to sheet - calculate speed here by maintaining
                 // prev x,y,z and then calculating it on an interval of 5 seconds
-                Log.e("log","Sensor1 data : "+x+" "+y+" "+z)
+
+
+                Log.e("log","Sensor1 data : "+x+" "+y+" "+z+" "+latitude+" "+longitude)
             }
         }
 
@@ -121,17 +221,56 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             val z = p0.values[2]
             if (tripTimer%5 ==0) {
                 // Add this data directly
-                Log.e("log","Sensor2 data : "+x+" "+y+" "+z)
+                Log.e("log","Sensor2 data : "+x+" "+y+" "+z+" "+latitude+" "+longitude)
             }
         }
     }
-    override fun onPause() {
-        super.onPause()
-        disableSensors()
+
+    @SuppressLint("ObsoleteSdkInt")
+    fun checkPermissionForLocation(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED){
+                true
+            }else{
+                // Show the permission request
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSION_LOCATION)
+                false
+            }
+        } else {
+            true
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        enableSensors()
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //We have to add startlocationUpdate() method later instead of Toast
+                Toast.makeText(this,"Permission granted",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+    private fun buildAlertMessageNoGps() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+            .setCancelable(false)
+            .setPositiveButton("Yes") { dialog, id ->
+                startActivityForResult(
+                    Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    , 11)
+            }
+            .setNegativeButton("No") { dialog, id ->
+                dialog.cancel()
+                finish()
+            }
+        val alert: AlertDialog = builder.create()
+        alert.show()
+
+
+    }
+
 }
